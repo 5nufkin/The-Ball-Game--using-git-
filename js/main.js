bd5c13c3f0bda7317e6e7dacbed6ef033f627ef9
Nir Sultan <snufkin62@gmail.com>
2025-03-18 14:42:59 +0200

    Add ball grow behavior

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 'use strict'
 
-function onBallClick() {
-  console.log('Hi')
+var gBallSize = 100
+
+function onBallClick(elBall) {
+  gBallSize = gBallSize + 50
+  elBall.style.width = gBallSize + 'px'
+  elBall.style.height = gBallSize + 'px'
+  elBall.innerText = gBallSize
 }
