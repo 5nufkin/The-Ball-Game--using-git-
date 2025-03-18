4cc0c0e221056a1fda97905dcbcc9f0c31d025a0
Nir Sultan <snufkin62@gmail.com>
2025-03-19 00:16:41 +0200

    Add shrink and Fix balls array

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@
 const gBalls = []
 gBalls[0] = { id: 1, color: 'orange', size: 100 }
 gBalls[1] = { id: 2, color: 'lightseagreen', size: 100 }
-gBalls[2] = { id: 3, color: 'steelblue', size: 100 }
 
 function onBallClick(elBall, maxDiameter) {
   const ball = gBalls[getBallIdx(elBall)]
@@ -45,9 +44,31 @@ function onSwap() {
   tempBall.size = ball1.size
   tempBall.color = ball1.color
 
-  changeBallSize(ball1, (ball2.size-ball1.size))
+  changeBallSize(ball1, (ball2.size - ball1.size))
   changeBallColor(ball1, ball2.color)
 
-  changeBallSize(ball2, tempBall.size-ball2.size)
+  changeBallSize(ball2, tempBall.size - ball2.size)
   changeBallColor(ball2, tempBall.color)
+}
+
+function onShrinkBalls() {
+  // const ball1 = gBalls[0]
+  // const ball2 = gBalls[1]
+
+  // changeBallSize(ball1, -getRandomIntInclusive(20, 60))
+  // changeBallSize(ball2, -getRandomIntInclusive(20, 60))
+
+  // if (ball1.size)
+
+    for (var i = 0; i < gBalls.length; i++) {
+      const currBall = gBalls[i]
+      changeBallSize(currBall, -getRandomIntInclusive(20, 60))
+      if (currBall.size < 100) {
+        currBall.size = 100
+        const elBall = getElBall(currBall)
+        elBall.style.width = currBall.size + 'px'
+        elBall.style.height = currBall.size + 'px'
+        elBall.innerText = currBall.size
+      }
+    }
 }
