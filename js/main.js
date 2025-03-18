0c8d37596a2cee76452cd0efc05497706c902e4d
Nir Sultan <snufkin62@gmail.com>
2025-03-18 14:49:16 +0200

    Limit ball size

--- js/main.js
+++ js/main.js
@@ -4,6 +4,9 @@ var gBallSize = 100
 
 function onBallClick(elBall) {
   gBallSize = gBallSize + 50
+  if (gBallSize === 450) {
+    gBallSize = 100
+  }
   elBall.style.width = gBallSize + 'px'
   elBall.style.height = gBallSize + 'px'
   elBall.innerText = gBallSize
