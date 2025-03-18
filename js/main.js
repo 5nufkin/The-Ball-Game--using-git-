d6daf2b1fa1a765f35b6ad14972d6efba29144be
Nir Sultan <snufkin62@gmail.com>
2025-03-18 17:26:42 +0200

    Fix ball size reset bug

--- js/main.js
+++ js/main.js
@@ -1,15 +1,29 @@
 'use strict'
 
-var gBallSize = 100
+var gBall1Size = 100
+var gBall2Size = 100
 
 function onBallClick(elBall, maxDiameter) {
-  const randSize = getRandomIntInclusive(20,60)
-  gBallSize = gBallSize + randSize
-  if (gBallSize >= maxDiameter) {
-    gBallSize = 100
+  var currBallSize
+  if (elBall.classList.contains('ball1')) {
+    currBallSize = gBall1Size
+  } else if (elBall.classList.contains('ball2')) {
+    currBallSize = gBall2Size
   }
-  elBall.style.width = gBallSize + 'px'
-  elBall.style.height = gBallSize + 'px'
-  elBall.innerText = gBallSize
+
+  const randSize = getRandomIntInclusive(20,60)
+  currBallSize = currBallSize + randSize
+  if (currBallSize >= maxDiameter) {
+    currBallSize = 100
+  }
+
+  elBall.style.width = currBallSize + 'px'
+  elBall.style.height = currBallSize + 'px'
+  elBall.innerText = currBallSize
   elBall.style.backgroundColor = getRandomColor()
+  if (elBall.classList.contains('ball1')) {
+    gBall1Size = currBallSize
+  } else if (elBall.classList.contains('ball2')) {
+    gBall2Size = currBallSize
+  }
 }
